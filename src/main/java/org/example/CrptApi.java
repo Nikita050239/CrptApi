e9398845a983c2330c337f29d98f7e21c4e3ef85
Nikita050239 <Nagorn89875@gmail.com>
2024-06-24 15:55:42 +0300

    Update CrptApi.java

--- src/main/java/org/example/CrptApi.java
+++ src/main/java/org/example/CrptApi.java
@@ -12,7 +12,6 @@ import java.net.URLConnection;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.util.List;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.Semaphore;
@@ -79,58 +78,6 @@ String text = downloadWebPage("https://ismp.crpt.ru/api/v3/lk/documents/create")
             System.err.println("Ошибка при отправке запроса: " + e.getMessage());
         } finally {
             semaphore.release();
-        }
-    }
-
-    public class Document {
-
-        private Description description;
-        private String doc_id;
-        private String doc_status;
-        private String doc_type;
-        private boolean importRequest;
-        private String owner_inn;
-        private String participant_inn;
-        private String producer_inn;
-        private String production_date;
-        private String production_type;
-        private List<Product> products;
-        private String reg_date;
-        private String reg_number;
-
-        public Document(Description description, String doc_id, String doc_status, String doc_type,
-                        boolean importRequest, String owner_inn, String participant_inn, String producer_inn,
-                        String production_date, String production_type, List<Product> products, String reg_date,
-                        String reg_number) {
-            this.description = description;
-            this.doc_id = doc_id;
-            this.doc_status = doc_status;
-            this.doc_type = doc_type;
-            this.importRequest = importRequest;
-            this.owner_inn = owner_inn;
-            this.participant_inn = participant_inn;
-            this.producer_inn = producer_inn;
-            this.production_date = production_date;
-            this.production_type = production_type;
-            this.products = products;
-            this.reg_date = reg_date;
-            this.reg_number = reg_number;
-        }
-
-        public static class Description {
-            private String participantInn;
-        }
-
-        public static class Product {
-            private String certificate_document;
-            private String certificate_document_date;
-            private String certificate_document_number;
-            private String owner_inn;
-            private String producer_inn;
-            private String production_date;
-            private String tnved_code;
-            private String uit_code;
-            private String uitu_code;
         }
     }
 }
